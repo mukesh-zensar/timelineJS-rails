3427555f6ee7f3bd3ce031fdf1a4f0f24f4a7dd9
Mbhati-Zensar <mukesh.bhati@zensar.in>
2017-05-16 21:56:27 +0530

    Removed unwanted code

--- lib/timelineJS/rails/engine.rb
+++ lib/timelineJS/rails/engine.rb
@@ -1,10 +1,6 @@
 module TimelineJS
   module Rails
     class Engine < ::Rails::Engine
-    	#adding to main app
-    	TimelineJS::Rails.setup do |config|
-    	  config.app_root = app.root
-    	end
     end
   end
 end
